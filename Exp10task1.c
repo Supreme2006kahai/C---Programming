8c0a1262b8a388aa663c682dbc1444c951a3123e
Supreme2006kahai <supreme@eng.rizvi.edu.in>
2024-11-28 20:23:44 +0530

    Exp10task1.c

--- Exp10task1.c
+++ Exp10task1.c
@@ -3,37 +3,21 @@ Roll No  : 9
 UIN      : 241A009
 Division : A*/
 #include <stdio.h>
-
 int main() {
-
-    int n;
-
+  int n;
     printf("\t\t** Array Reverser ** \n\n");
-
-
     printf("Enter the size of the array: ");
     scanf("%d", &n);
-
     int arr[n];
-
     int *ptr = arr;
-
-
     printf("Enter the elements of the array:\n");
-
     for (int i = 0; i < n; i++) {
-
         scanf("%d", ptr + i);
     }
-
-
     printf("\nArray elements in reverse order:\n");
-
     for (int i = n - 1; i >= 0; i--) {
         printf("%d ", *(ptr + i));
     }
-
     printf("\n");
-
     return 0;
 }
